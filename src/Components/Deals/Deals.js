3a65b793475d43d38c6974baf359d687f1ca1e56
Md.Rabiul islam <rabiulrabiul1924@gmail.com>
2021-04-11 18:15:52 +0600

    final

--- src/Components/Deals/Deals.js
+++ src/Components/Deals/Deals.js
@@ -2,9 +2,8 @@ import React from 'react';
 
 const Deals = () => {
     return (
-        <div style={{textAlign: 'center' , color: 'red',marginTop:"100px"}}>
-            <h2>Deals is Under Construction</h2>
-            <h2>Please comeback letter....</h2>
+        <div >
+            Deal on going
         </div>
     );
 };
